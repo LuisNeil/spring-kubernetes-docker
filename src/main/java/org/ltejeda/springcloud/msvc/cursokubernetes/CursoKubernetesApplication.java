7647bd6cd56503f4e1ae4d386fd48297593f0c59
Luis Neil <lntejedal@gmail.com>
2022-10-31 14:41:41 -0500

    Crear msvc

---
+++ src/main/java/org/ltejeda/springcloud/msvc/cursokubernetes/CursoKubernetesApplication.java
@@ -0,0 +1,13 @@
+package org.ltejeda.springcloud.msvc.cursokubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoKubernetesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoKubernetesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ltejeda/springcloud/msvc/cursokubernetes/CursoKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ltejeda.springcloud.msvc.cursokubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoKubernetesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
